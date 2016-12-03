543798eccf5ce880c12264ee4f2e255881390880
OhmGeek <ryan@ohmgeek.co.uk>
2016-12-03 13:19:06 +0000

    Phone!

---
+++ src/phone.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, redirect
+import twilio.twiml
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET','POST'])
+def respondToPerson():
+	fromContact = request.values.get('From',None);
+	
+	#get a response from the Trump API
+
+
+	#send info to the person
+	resp = twilio.twiml.Response()
+	resp.message("hello")
+
+	return str(resp)
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port="5000")
