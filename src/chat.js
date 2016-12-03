c9cd6a697c182b3b81e9ca325c10ad935fec173f
OhmGeek <ryan@ohmgeek.co.uk>
2016-12-03 15:23:36 +0000

    Broken TF, Keras attempt, new JS imp

--- src/chat.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('chatDisplay',[]);
-
-app.controller('chatCtrl', function($scope) {
-	$scope.records = [];
-	$scope.addChat = function(message) {
-		$scope.records.push(message);
-		$scope.$apply();
-	};
-});
--- src/trumpGen.py
+++ src/trumpGen.py
@@ -9,24 +9,32 @@ class TrumpGenerator(object):
 	def __init__(self):
 		self.maxLength = 10
 		self.path = "tweets.txt"
+		self.charIDXFile = "char_idx.pickle"
 
 	def train(self):
+
 		char_idx = None
+
+		if(os.path.isfile(self.charIDXFile)):
+			# load previous character file
+			char_idx = pickle.load(open(self.charIDXFile, 'rb'))
+
+
 		X, Y, char_idx = textfile_to_semi_redundant_sequences(self.path,seq_maxlen=self.maxLength,redun_step=3)
 
-		g = tflearn.input_data([None,self.maxLength,len(char_idx)]);
-		g = tflearn.lstm(g,512,return_seq=True)
-		g = tflearn.dropout(g,0.5)
-		g = tflearn.lstm(g,512,return_seq=True)
-		g = tflearn.dropout(g,0.5)
-		g = tflearn.lstm(g,512)
-		g = tflearn.dropout(g,0.5)
-		g = tflearn.fully_connected(g,len(char_idx),activation='softmax')
-		g = tflearn.regression(g, optimizer='adam', loss='categorical_crossentropy',
+		pickle.dump(char_idx, open(self.charIDXFile, 'wb'))
+
+		self.g = tflearn.input_data([None,self.maxLength,len(char_idx)]);
+		self.g = tflearn.lstm(self.g,512,return_seq=True)
+		self.g = tflearn.dropout(self.g,0.5)
+		self.g = tflearn.lstm(self.g,512,return_seq=True)
+		self.g = tflearn.dropout(self.g,0.5)
+		self.g = tflearn.lstm(self.g,512)
+		self.g = tflearn.dropout(self.g,0.5)
+		self.g = tflearn.fully_connected(self.g,len(char_idx),activation='softmax')
+		self.g = tflearn.regression(self.g, optimizer='adam', loss='categorical_crossentropy',
 								 learning_rate=0.001)
-		self.model = tflearn.SequenceGenerator(g, dictionary=char_idx, seq_maxlen=self.maxLength,
-												clip_gradients=5.0, 
-												checkpoint_path='model_trump')
+		self.model = tflearn.SequenceGenerator(self.g, dictionary=char_idx, seq_maxlen=self.maxLength, max_checkpoints=0,checkpoint_path=0,session=None)
 
 	def getResponse(self,input="wall"):
 		m.fit(X,Y,validation_set=0.1,batch_size=128,n_epoch=1,run_id='trumpedupkicks')
