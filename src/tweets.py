dd43e8bf6b83f07ca425bd09ab50f8940e60200c
mikecroall <mikebcroall@gmail.com>
2016-12-03 11:46:01 +0000

    tweets.py now runs - testing in progress

--- src/tweets.py
+++ src/tweets.py
@@ -1,5 +1,5 @@
-from twython import Twython # pip install twython
-import time # standard lib
+from twython import Twython  # pip install twython
+import time  # standard lib
 from apiKeys import CK, CS, AT, ATS
 
 ''' Go to https://apps.twitter.com/ to register your app to get your api keys '''
@@ -7,24 +7,22 @@ CONSUMER_KEY = CK
 CONSUMER_SECRET = CS
 ACCESS_KEY = AT
 ACCESS_SECRET = ATS
-twitter = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
+twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 
-user_timeline = twitter.get_user_timeline(screen_name="craigaddyman",count=1)
-latest_id = user_timeline['id']
+user_timeline = twitter.get_user_timeline(screen_name="craigaddyman", count=1)
+latest_id = user_timeline[0]['id']
 
-tweetIDs = [latest_id] ## this is the latest starting tweet id
-for i in range(0, 16): ## iterate through all tweets
-    print("Loop " + i)
+tweetIDs = [latest_id]  ## this is the latest starting tweet id
+for i in range(0, 16):  ## iterate through all tweets
+    print("Loop " + str(i))
     ## tweet extract method with the last list item as the max_id
     user_timeline = twitter.get_user_timeline(screen_name="realDonaldTrump",
                                               count=200, include_retweets=False, max_id=tweetIDs[-1])
-    time.sleep(300) ## 5 minute rest between api calls
+    time.sleep(300)  ## 5 minute rest between api calls
 
     for tweet in user_timeline:
-        print(tweet['text']) ## print the tweet
-        tweetIDs.append(tweet['id']) ## append tweet id's
+        print(tweet['text'])  ## print the tweet
+        tweetIDs.append(tweet['id'])  ## append tweet id's
 
-
-
-print("Total tweet count: " + len(tweetIDs))
-print("Last tweet ID: " + tweetIDs[-1])
+print("Total tweet count: " + str(len(tweetIDs)))
+print("Last tweet ID: " + str(tweetIDs[-1]))
