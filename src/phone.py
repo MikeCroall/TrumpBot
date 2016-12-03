ace6ff1ffa10bb529b95c4bb83150e3a02ac8a18
OhmGeek <ryan@ohmgeek.co.uk>
2016-12-03 19:27:22 +0000

    Twilio works

--- src/phone.py
+++ src/phone.py
@@ -1,18 +1,19 @@
 from flask import Flask, request, redirect
 import twilio.twiml
-
+import requests
 app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def respondToPerson():
 	fromContact = request.values.get('From',None);
-	inputMessage = request.values.get('Body',None);	
+	inputMessage = request.values.get('Body',None);
+	param = {'q':inputMessage}
 	#get a response from the Trump API
-
+	res = requests.get('http://32d4b85d.ngrok.io/response',param)
 
 	#send info to the person
 	resp = twilio.twiml.Response()
-	resp.message(inputMessage)
+	resp.message(res.text)
 
 	return str(resp)
 
