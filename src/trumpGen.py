97058b4e6e4e8e7cc0d0137948ceda6c769bb3fb
OhmGeek <ryan@ohmgeek.co.uk>
2016-12-03 17:49:34 +0000

    trump is alive

--- src/trumpGen.py
+++ src/trumpGen.py
@@ -1,19 +1,20 @@
 import os
+from senGen import SentenceGenerator
 
 
 class TrumpGenerator(object):
-	def __init__(self,markovLength):
-		self.gen = SentenceGenerator()
-		self.markovLength = markovLength
-	
-	def train(self,filename):
-		self.gen.buildMapping(self.gen.wordlist(filename),self.markovLength)	
 
-	def getResponse(self,input="wall"):
-		return genSentence(markovLength)
+    def __init__(self, markovLength):
+        self.gen = SentenceGenerator()
+        self.markovLength = markovLength
+
+    def train(self, filename):
+        self.gen.buildMapping(self.gen.wordlist(filename), self.markovLength)
+
+    def getResponse(self, inputWord="wall"):
+        return self.gen.genSentence(self.markovLength, inputWord)
 
 if __name__ == "__main__":
-	tg = TrumpGenerator()
-	tg.train()
-	print(tg.getResponse())
-
+    tg = TrumpGenerator(1)
+    tg.train("tweets.txt")
+    print(tg.getResponse())
