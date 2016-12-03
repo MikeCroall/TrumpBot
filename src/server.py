0f814a83eea2fb229733d81b95f017c3d1ad9aa7
OhmGeek <ryan@ohmgeek.co.uk>
2016-12-03 18:23:54 +0000

    Server working for one word query

--- src/server.py
+++ src/server.py
@@ -1,21 +1,37 @@
 from flask import Flask, request, redirect, url_for
 from trumpGen import TrumpGenerator
+import re
+import random
+import sys
 
 app = Flask(__name__)
+tg = None
+stopList = ['a','an','and','are','as','at','be','by','for','from','has','he','in','is','it','its','of','on','that','the','to','was','were','will','with']
+
+def run_on_start():
+	global tg
+	tg = TrumpGenerator(4)	
+	tg.train("tweets.txt")
 
 @app.route('/')
 def root():
 	return "API Working"
 
-@app.route('/response')
+@app.route('/response', methods=['GET','POST'])
 def getResponse():
-	if(request.args['q']):
-		//use TF on model
+	if(request.args.get('q') != None):
+		return tg.getResponse(request.args.get('q'))
 	else:
-		//generate random trump sentence statically
+		#generate random trump sentence statically
+		return tg.getResponse()
 
+def processInput(inputStr):
+	#strip punctuation
 
+	#remove stop words
+
+	#pick a random word
 
 if __name__ == '__main__':
+	run_on_start()
 	app.run(host='0.0.0.0', port=3000)
-
